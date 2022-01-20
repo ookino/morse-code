7f0a9c904622afeed0d4e3b66bb6dc882f81311f
Okino <37089367+yaseerokino@users.noreply.github.com>
2022-01-20 19:55:52 +0100

    Create decode.rb

---
+++ decode.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+MORSE = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(char)
+  if MORSE.key? char
+    MORSE[char]
+  else
+    'Input is not morse code'
+  end
+end
+
+def dedcode_word(word)
+  word_arr = []
+  word.split.each do |n|
+    word_arr.push(MORSE[n])
+  end
+  word_arr.join
+end
+
+
